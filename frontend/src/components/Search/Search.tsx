0a513dca0323c7249bc6f11a7f6d891c8d63c88b
Devin Jaggernauth <neexis@gmail.com>
2023-01-30 21:07:59 -0400

    setup form validation and error message for search page

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Search.css';
 import searchDecoration from '../../images/spiral-down-pink.png';
 import searchPet from '../../images/search-pet.png';
@@ -15,7 +16,7 @@ const Search = ({ setSearchResults }: { setSearchResults: Function }) => {
   const {
     register,
     handleSubmit,
-    formState: { isValid, isDirty },
+    formState: { errors, isValid, isDirty },
     formState,
     reset,
   } = useForm({
@@ -25,19 +26,28 @@ const Search = ({ setSearchResults }: { setSearchResults: Function }) => {
     mode: 'onChange',
   });
 
+  const [errorMessage, setErrorMessage] = useState('');
+
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
       reset({ query: '' });
+    }
+    if (formState.isDirty) {
+      setErrorMessage('');
     }
   }, [formState, reset]);
 
   const navigation = useNavigate();
 
   const submitSearchData = ({ query }: SearchQuery) => {
-    getPetByLocation(query).then((data) => {
-      setSearchResults(data);
-      navigation('/adopt');
-    });
+    getPetByLocation(query)
+      .then((data) => {
+        setSearchResults(data);
+        navigation('/adopt');
+      })
+      .catch(() => {
+        setErrorMessage('No pets found for this city. Try another');
+      });
   };
   return (
     <FormPage
@@ -50,10 +60,17 @@ const Search = ({ setSearchResults }: { setSearchResults: Function }) => {
           Look for pets in your area
         </label>
         <input
-          {...register('query', { required: true })}
+          {...register('query', {
+            required: 'Please enter a city',
+            minLength: {
+              value: 2,
+              message: 'City name must be at least 2 characters',
+            },
+          })}
           className="form__input"
           placeholder="Type your city"
         />
+        <p className="form__error">{errors.query?.message || errorMessage}</p>
         <input
           type="submit"
           className={`form__submit form__submit_search ${
