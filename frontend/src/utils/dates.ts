62ae308432e45f5d1709190ee56d96a5b7195f95
Devin Jaggernauth <neexis@gmail.com>
2023-02-07 20:41:45 -0400

    update pet card  buttons

--- frontend/src/utils/dates.ts
+++ frontend/src/utils/dates.ts
@@ -1,20 +1,19 @@
 const ONE_MONTH_IN_MILLISECONDS = 2629746000;
-const ONE_DAY_IN_MILLISECONDS = 86400000
+const ONE_WEEK_IN_MILLISECONDS = 604800000;
 const MONTHS_IN_ONE_YEAR = 12;
 
 export const calculateCurrentAge = (dateString: string) => {
   const dateOfBirth = Date.parse(dateString);
   const today = new Date().getTime();
-  const ageInMonths = Math.round(
-    (today - dateOfBirth) / ONE_MONTH_IN_MILLISECONDS
-  );
+  const ageInMilliseconds = today - dateOfBirth;
+  const ageInWeeks = Math.round(ageInMilliseconds / ONE_WEEK_IN_MILLISECONDS);
+  const ageInMonths = Math.floor(ageInMilliseconds / ONE_MONTH_IN_MILLISECONDS);
   const ageInYears = Math.floor(ageInMonths / MONTHS_IN_ONE_YEAR);
   const remainderMonths = ageInMonths % MONTHS_IN_ONE_YEAR;
 
+  if (ageInMilliseconds < 0) return;
+  if (ageInWeeks < 9) return `${ageInWeeks} wks`;
   if (ageInMonths < 12) return `${ageInMonths} mo`;
-
-  if (remainderMonths === 0) return `${ageInYears} years`;
+  if (remainderMonths === 0) return `${ageInYears} y`;
   else return `${ageInYears} y, ${remainderMonths} mo`;
 };
-
-
