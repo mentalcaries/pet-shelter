1c318d2b35efa81c0a981db6a5dbce12828eafc6
Devin Jaggernauth <neexis@gmail.com>
2023-03-07 14:56:43 -0400

    create user checking for invalid fields

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,6 +1,6 @@
 import { prisma } from './client';
 import { NextFunction, Request, Response } from 'express';
-import { hashPassword } from '../utils/authUtils'
+import { hashPassword } from '../utils/authUtils';
 
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
@@ -9,14 +9,18 @@ const createUser = catchAsync(
   async (request: Request, response: Response, next: NextFunction) => {
     const { username, name, email, password, type, city, country } =
       request.body;
+
+    if (!username || !email || !name || !password || !type)
+      throw new AppError('Some fields are missing', 400);
+
     const existingUser = await prisma.user.findFirst({
       where: {
-        OR: [{username}, {email}]
-      } 
+        OR: [{ username }, { email }],
+      },
     });
-    if (existingUser) next(new AppError('User already exists', 409))
+    if (existingUser) throw new AppError('User already exists', 409);
 
-    const hashedPassword = await hashPassword(password)
+    const hashedPassword = await hashPassword(password);
     const user = await prisma.user.create({
       data: {
         username,
@@ -33,7 +37,10 @@ const createUser = catchAsync(
     else {
       return response
         .status(200)
-        .json({ Message: 'Successful registration', result: { username, email, type, city, country} });
+        .json({
+          Message: 'Successful registration',
+          result: { username, email, type, city, country },
+        });
     }
   }
 );
